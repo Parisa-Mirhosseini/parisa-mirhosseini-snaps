c655bbf0e2f5a8b4d23e881f928330f9f5354b29
parisa-mirhosseini-snaps <parisamirhosseini2@gmail.com>
2025-02-17 19:48:07 -0500

    fixin styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import "../Footer/Footer.scss";
 
+import Facebook from "../../assets/images/Facebook.svg";
+import Twitter from "../../assets/images/X_twitter.svg";
+import Instagram from "../../assets/images/Instagram.svg";
+import Pinterest from "../../assets/images/Pinterest.svg";
+
 function Footer() {
 
     return (
@@ -9,22 +14,33 @@ function Footer() {
                 <h1>Snaps</h1>
                 <h4>For Photographers</h4>
                 <h4>Hire a Talent</h4>
-                <h4>Inspiration</h4>
+                <h4 className="footer--inspiration">Inspiration</h4>
                 <h4>About</h4>
                 <h4>Careers</h4>
                 <h4>Support</h4>
-                <a href="http://www.facebook.com">
-                    <img src="" alt="Facebook" />
-                </a>
-                <a href="http://www.twitter.com">
-                    <img src="/path/to/twitter-icon.png" alt="Twitter" />
-                </a>
-                <a href="http://www.instgram.com">
-                    <img src="/" alt="Instagram" />
-                </a>
-                <a href="http://www.pintrest.com">
-                    <img src="/path/to/pinterest-icon.png" alt="Pinterest" />
-                </a>
+                <div className="footer__bottom">
+                <div className="footer__bottom--socials">
+                    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                        <img src={Facebook} alt="Facebook" />
+                    </a>
+                    <a href="https://x.com/" target="_blank" rel="noopener noreferrer">
+                        <img src={Twitter} alt="Twitter" />
+                    </a>
+                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                        <img src={Instagram} alt="Instagram" />
+                    </a>
+                    <a href="https://www.pinterest.com/" target="_blank" rel="noopener noreferrer">
+                        <img src={Pinterest} alt="Pinterest" />
+                    </a>
+                </div>
+                <div className="footer__bottom--copyright">
+                    <p>&copy; 2024 Snaps</p>
+                    <p>.Terms</p>
+                    <p>Privacy</p>
+                    <p>Cookies</p>
+
+                </div>
+                </div>
             </div>
 
         </>);
